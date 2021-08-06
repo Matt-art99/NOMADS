479603c682ef288a779799a32cb58241a06ac828
Rachmat Ardico <rahmatardiko99@gmail.com>
2021-08-07 02:08:30 +0700

    Install dan mengatur autentikasi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\{CheckoutController, DetailControler ,HomeController};
@@ -11,7 +12,10 @@ Route::get('/detail', [DetailControler::class, 'index'])->name('detail');
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware(['auth','admin'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
+
+
+Auth::routes();
 
